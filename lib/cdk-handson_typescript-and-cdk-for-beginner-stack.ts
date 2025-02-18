964a819b9143c1f93a354871ca8ac85fbc813b4f
t-takahashi <tttol.t4k.b1z@gmail.com>
2025-02-19 06:59:23 +0900

    create ec2

--- lib/cdk-handson_typescript-and-cdk-for-beginner-stack.ts
+++ lib/cdk-handson_typescript-and-cdk-for-beginner-stack.ts
@@ -1,19 +1,31 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
+import { readFileSync } from 'fs';
 
 export class CdkHandsonTypescriptAndCdkForBeginnerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    const vpc = new ec2.Vpc(this, 'BlogVpc', {ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16')});
 
-    const queue = new sqs.Queue(this, 'CdkHandsonTypescriptAndCdkForBeginnerQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // AmazonLinux2, t3.small
+    const webServer1 = new ec2.Instance(this, "WordpressSever1", {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      vpcSubnets: {subnetType: ec2.SubnetType.PUBLIC},
     });
 
-    const topic = new sns.Topic(this, 'CdkHandsonTypescriptAndCdkForBeginnerTopic');
+    // install wordpress etc...(yum install)
+    const script = readFileSync("./lib/resource/user-data.sh", "utf-8");
+    webServer1.addUserData(script);
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+
+    new CfnOutput(this, "WordpressServer1PublicIPAddress", {
+      value: `http://${webServer1.instancePublicIp}`,
+    })
   }
 }
