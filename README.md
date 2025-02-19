a9ac1fdd1728b45bb0f44af5793a00e696db9056
t-takahashi <tttol.t4k.b1z@gmail.com>
2025-02-20 06:24:40 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+# Overview
 下記ハンズオン「TypeScript の基礎から始める AWS CDK 開発入門」の実施記録  
 https://catalog.workshops.aws/typescript-and-cdk-for-beginner/ja-JP
+
+# memo
+- ドキュメントの見方
+  - まずは各サービスの[Overview](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_rds-readme.html)をチェック
+  - 建てたいリソースを探して、[Constructs](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_rds.DatabaseInstance.html)の章から探す
+- RDSを立ち上げるとシークレットが自動的にSecretManagerに保存される
--- lib/cdk-handson_typescript-and-cdk-for-beginner-stack.ts
+++ lib/cdk-handson_typescript-and-cdk-for-beginner-stack.ts
@@ -1,11 +1,19 @@
 import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
 import { Construct } from 'constructs';
 import { readFileSync } from 'fs';
 
 export class CdkHandsonTypescriptAndCdkForBeginnerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // EC2 インスタンス のリソースタイプは AWS::EC2::Instance です
+    // インスタンスタイプには t3.small を指定します
+    // マシンイメージには Amazon Linux 2 を指定します
+    // EC2 インスタンスはパブリックサブネットに配置します
+    // ユーザーデータを利用して WordPress をインストールします
+    // セキュリティグループでインターネットからの HTTP 80 番ポートへのアクセスを許可します
     const vpc = new ec2.Vpc(this, 'BlogVpc', {ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16')});
 
     // AmazonLinux2, t3.small
@@ -19,7 +27,7 @@ export class CdkHandsonTypescriptAndCdkForBeginnerStack extends Stack {
     });
 
     // install wordpress etc...(yum install)
-    const script = readFileSync("./lib/resource/user-data.sh", "utf-8");
+    const script = readFileSync("./lib/resources/user-data.sh", "utf-8");
     webServer1.addUserData(script);
 
     webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
@@ -27,5 +35,19 @@ export class CdkHandsonTypescriptAndCdkForBeginnerStack extends Stack {
     new CfnOutput(this, "WordpressServer1PublicIPAddress", {
       value: `http://${webServer1.instancePublicIp}`,
     })
+
+    // RDS DB インスタンス のリソースタイプは AWS::RDS::DBInstance です
+    // データベースエンジンは MySQL の version 8.0.36 を使用します
+    // RDS DB インスタンスのインスタンスタイプには t3.small を指定します
+    // RDS DB インスタンスはプライベートサブネットにデプロイします
+    // データベース名には wordpress を指定します
+    // EC2 インスタンスから RDS DB インスタンスへのアクセスを許可します
+    const db = new rds.DatabaseInstance(this, "WordPressDB", {
+      engine: rds.DatabaseInstanceEngine.mysql({version: rds.MysqlEngineVersion.VER_8_0_36}),
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
+      databaseName: "wordpress",
+    })
+    db.connections.allowDefaultPortFrom(webServer1);
   }
 }
